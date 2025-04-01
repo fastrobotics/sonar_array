dd6065c6da171c8298e566f9dc82bfd80137ba23
David Gitz <davidgitz@gmail.com>
2025-03-31 20:47:02 -0500

    Major work done.

--- nodes/SonarArrayDriverNode/node/SonarArrayDriverNode.h
+++ nodes/SonarArrayDriverNode/node/SonarArrayDriverNode.h
@@ -35,10 +35,10 @@ class SonarArrayDriverNode : public eros::BaseNode
     const uint16_t MINOR_RELEASE_VERSION = 0;
 
     /*! \brief The Build Number of the Node.*/
-    const uint16_t BUILD_NUMBER = 3;
+    const uint16_t BUILD_NUMBER = 4;
 
     /*! \brief A Description of the Firmware.*/
-    const std::string FIRMWARE_DESCRIPTION = "Latest Rev: 19-Mar-2025";
+    const std::string FIRMWARE_DESCRIPTION = "Latest Rev: 31-Mar-2025";
 
     /*! \brief What System this Node falls under.*/
     const eros::System::MainSystem DIAGNOSTIC_SYSTEM = eros::System::MainSystem::ROVER;
